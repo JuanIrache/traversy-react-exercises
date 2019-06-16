3d613cabd510ff524bdaef3032c37a904f6d5257
JuanIrache <yrache@gmail.com>
2019-06-17 00:56:01 +0200

    fetch github users with axios and pass them from App.js to Users.js

--- project1/src/App.js
+++ project1/src/App.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- project1/src/components/users/Users.js
+++ project1/src/components/users/Users.js
@@ -2,32 +2,10 @@ import React, { Component } from 'react';
 import UserItem from './UserItem';
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        login: 'errfree',
-        id: 44,
-        avatar_url: 'https://avatars2.githubusercontent.com/u/44?v=4',
-        html_url: 'https://github.com/errfree'
-      },
-      {
-        login: 'mojodna',
-        id: 45,
-        avatar_url: 'https://avatars2.githubusercontent.com/u/45?v=4',
-        html_url: 'https://github.com/mojodna'
-      },
-      {
-        login: 'bmizerany',
-        id: 46,
-        avatar_url: 'https://avatars2.githubusercontent.com/u/46?v=4',
-        html_url: 'https://github.com/bmizerany'
-      }
-    ]
-  };
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => {
+        {this.props.users.map(user => {
           return <UserItem key={user.id} user={user} />;
         })}
       </div>
