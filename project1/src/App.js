66b0ec327e3a30806717db0eeeef1cb18f568e5b
JuanIrache <yrache@gmail.com>
2019-06-17 00:09:12 +0200

    add user component with state

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
+        <UserItem />
       </div>
     );
   }
--- project1/src/components/layout/Navbar.js
+++ project1/src/components/layout/Navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
+class Navbar extends Component {
   static defaultProps = {
     title: 'Missing Title',
     icon: 'fas fa-times'
