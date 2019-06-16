2ea65bb3f0befaa7955ce9d5bbe67a08c3255763
JuanIrache <yrache@gmail.com>
2019-06-17 00:38:16 +0200

    adding file from old commit 66b0ec3

---
+++ project1/src/components/users/UserItem.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class UserItem extends Component {
+  state = {
+    login: 'defunkt',
+    id: 2,
+    avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
+    html_url: 'https://github.com/defunkt'
+  };
+  render() {
+    const { login, avatar_url, html_url } = this.state;
+    return (
+      <div className="card text-center">
+        <img src={avatar_url} alt="" className="round-img" style={{ width: '60px' }} />
+        <h3>{login}</h3>
+        <div>
+          <a href={html_url} className="btn btn-dark btn-sm my-1">
+            More
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserItem;
