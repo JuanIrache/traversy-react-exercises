1dd1260676877c60477cbf2181c7139956e03546
JuanIrache <yrache@gmail.com>
2019-06-17 13:55:40 +0200

    pass searchUsers as prop and retrieve search from github api

--- project1/src/App.js
+++ project1/src/App.js
@@ -19,12 +19,21 @@ class App extends Component {
     );
     this.setState({ users: res.data, loading: false });
   }
+  searchUsers = async text => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_ID}&client_secret=${
+        process.env.REACT_APP_GITHUB_CLIENT_SECRET
+      }`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
         <div className="container">
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- project1/src/components/users/Search.js
+++ project1/src/components/users/Search.js
@@ -9,7 +9,8 @@ class Search extends Component {
   };
   onSubmit = e => {
     e.preventDefault();
-    console.log(this.state.text);
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
   };
   render() {
     return (
