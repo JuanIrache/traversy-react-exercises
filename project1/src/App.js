eaba4e04c2105f66fc71c0f0ff928b13bc4979df
JuanIrache <yrache@gmail.com>
2019-06-17 13:22:09 +0200

    add search component and print input

--- project1/src/App.js
+++ project1/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 
 class App extends Component {
@@ -23,6 +24,7 @@ class App extends Component {
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
         <div className="container">
+          <Search />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
